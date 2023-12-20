f299539674e3cd34d208e46da569de28892c8444
AlviraAaisha <124269682+AlviraAaisha@users.noreply.github.com>
2023-12-20 18:39:46 +0000

    const,let , var

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,4 @@
+const accountId = 144788               //never goint to change
+let accountEmail ="aa@gmail.com"       //can b change
+var accountPassword = "12345"          //can be change, pefer not to use var
+accountCity = "Jaipur"                 //u can use it but not best way
